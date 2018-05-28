b88ebccfce065c9a2d0fa0bd920abcdcf378bf3f
max <mly520@gmail.com>
2018-05-28 06:58:08 +0000

    Auto translate slug

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Topic;
+use App\Handlers\SlugTranslateHandler;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -11,7 +12,13 @@ class TopicObserver
 {
     public function saving(Topic $topic)
     {
+        // XSS対応
         $topic->body = clean($topic->body, 'user_topic_body');
+        // 簡略文自動作成
         $topic->excerpt = make_excerpt($topic->body);
+        // slug内容自動生成
+        if ( ! $topic->slug) {
+            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+        }
     }
 }
